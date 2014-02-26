b9c815c0a4288027884023e30adf403bd2847624
Stef Horner <shorner@wearearchitect.com>
2014-02-26 08:58:00 +0000

    test slugifier

---
+++ tests/SlugifierTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use Mockery as m;
+
+class SlugifierTest extends \PHPUnit_Framework_TestCase {
+
+	public function setUp()
+	{
+		$this->slugifier = new Tlr\Support\Slugifier;
+	}
+
+	public function testNumericIncrementor()
+	{
+		$foo = 'foo';
+		$result = $this->slugifier->numericIncrementor($foo, 1);
+		$this->assertEquals('foo-1', $result);
+	}
+
+	public function testGetDefaultIncrementor()
+	{
+		$result = $this->slugifier->getIncrementor(null);
+		$this->assertEquals( array($this->slugifier, 'numericIncrementor') , $result);
+	}
+
+	public function testGetIncrementor()
+	{
+		$result = $this->slugifier->getIncrementor('foobar');
+		$this->assertEquals('foobar', $result);
+	}
+
+	public function testIncrementalSlug()
+	{
+		$input = 'woop';
+		$xi = 0;
+		$comparator = function($string) use (&$xi)
+		{
+			$xi++;
+			return $string === 'woop-4';
+		};
+
+		$result = $this->slugifier->incrementalSlug($input, $comparator);
+
+		$this->assertEquals('woop-4', $result);
+		$this->assertEquals(5, $xi);
+	}
+
+	public function testDefaultIncrementalSlug()
+	{
+		$input = 'woop';
+		$xi = 0;
+		$comparator = function($string) use (&$xi)
+		{
+			$xi++;
+			return true;
+		};
+
+		$result = $this->slugifier->incrementalSlug($input, $comparator);
+
+		$this->assertEquals('woop', $result);
+		$this->assertEquals(1, $xi);
+	}
+}
