e6c7dc15381222547ba25dc5276489d4fbd9333c
Stef Horner <stef@tedslittlerobot.com>
2014-02-27 23:26:19 +0000

    test basic enforcer enforce method

---
+++ tests/OrderEnforcerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Mockery as m;
+
+class OrderEnforcerTest extends \PHPUnit_Framework_TestCase {
+
+	public function setUp()
+	{
+		$this->orderer = m::mock('Tlr\Support\Database\Orderer');
+		$this->enforcer = new Tlr\Support\Database\OrderEnforcer($this->orderer);
+	}
+
+	public function testEnforce()
+	{
+		$array = array();
+		$array[] = $this->listItem(false, 1);
+		$array[] = $this->listItem(true, 3);
+		$array[] = $this->listItem(true, 5);
+		$array[] = $this->listItem(true, 8);
+
+		$return = $this->enforcer->enforce( $array );
+
+		$this->assertSame($array, $return);
+
+		foreach ($return as $key => $value)
+		{
+			$this->assertEquals( $key + 1, $value->index );
+		}
+	}
+
+	public function listItem( $dirty = true, $index = 1 )
+	{
+		$item = m::mock();
+
+		$item->shouldReceive('isDirty')->once()->with('index')->andReturn($dirty);
+
+		if ($dirty)
+		{
+			$item->shouldReceive('save')->once();
+		}
+
+		$item->index = $index;
+
+		return $item;
+	}
+
+}
