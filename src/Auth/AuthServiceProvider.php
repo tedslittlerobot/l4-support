15b1412f48ae256a7813a02226213bd93d4d6c54
Stef Horner <shorner@wearearchitect.com>
2014-04-08 10:13:33 +0100

    move routes to callable methods

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -33,7 +33,13 @@ class AuthServiceProvider extends ServiceProvider {
 
 		$this->userEvents( $this->app['events'] );
 
-		$this->routes( $this->app['events'] );
+		$events->listen('routes.start', array($this, 'routes'));
+		$events->listen('routes.admin-prefix', array($this, 'adminPrefixRoutes'));
+		$events->listen('routes.public', array($this, 'publicRoutes'));
+		$events->listen('routes.login-prefix', array($this, 'loginPrefixRoutes'));
+		$events->listen('routes.login', array($this, 'loginRoutes'));
+		$events->listen('routes.private', array($this, 'privateRoutes'));
+		$events->listen('routes.admin', array($this, 'adminRoutes'));
 
 		$this->filters( $this->app['router'] );
 
@@ -77,80 +83,74 @@ class AuthServiceProvider extends ServiceProvider {
 		});
 	}
 
-	/**
-	 * Register some routes
-	 * @param  Router $router
-	 */
-	public function routes( $events )
+	public function routes( $router, $events )
 	{
-		$events->listen('routes.start', function( $router ) use ( $events )
+		$router->group( [ 'before' => 'guest' ], function () use ( $router, $events )
 		{
-			$router->group( [ 'before' => 'guest' ], function () use ( $router, $events )
-			{
-				$events->fire('routes.public', array( $router, $events ));
-			} );
+			$events->fire('routes.public', array( $router, $events ));
+		} );
 
-			$router->group( ['before' => 'auth'], function() use ( $router, $events )
-			{
-				$events->fire('routes.private', array( $router, $events ));
-
-				$events->fire('routes.admin-prefix', array( $router, $events ));
-			} );
-		});
-
-		$events->listen('routes.admin-prefix', function($router, $events)
+		$router->group( ['before' => 'auth'], function() use ( $router, $events )
 		{
-			$router->group( ['prefix' => 'admin'], function() use ( $router, $events )
-			{
-				$events->fire('routes.admin', array( $router, $events ));
-			} );
-		});
+			$events->fire('routes.private', array( $router, $events ));
 
-		$events->listen('routes.public', function( $router, $events )
-		{
-			$events->fire('routes.login-prefix', array( $router, $events ) );
-		});
-
-		$events->listen('routes.login-prefix', function($router, $events)
-		{
-			$events->fire('routes.login', array( $router, $events ));
-		});
-
-		$events->listen('routes.login', function($router, $events)
-		{
-			$router->get('log/me/in', [ 'as' => 'login', 'uses' => 'Tlr\Auth\LoginController@loginForm' ]);
-			$router->post('i/am/important', [ 'as' => 'login.attempt', 'uses' => 'Tlr\Auth\LoginController@login', 'before' => 'csrf' ]);
-
-			// PASSWORD RESET
-
-			$router->get('reset/password/request', [ 'as' => 'password.request', 'uses' => 'Tlr\Auth\PasswordResetController@request' ]);
-			$router->post('reset/password/request', [ 'as' => 'password.request.process', 'uses' => 'Tlr\Auth\PasswordResetController@processRequest' ]);
-
-			$router->get('reset/password/{token}', [ 'as' => 'password.reset', 'uses' => 'Tlr\Auth\PasswordResetController@reset' ]);
-			$router->post('reset/password', [ 'as' => 'password.reset.process', 'uses' => 'Tlr\Auth\PasswordResetController@processReset' ]);
-		});
-
-		$events->listen('routes.private', function( $router )
-		{
-			$router->any('logout', [ 'as' => 'logout', 'uses' => 'Tlr\Auth\LoginController@logout' ]);
-		});
-
-		$events->listen('routes.admin', function( $router )
-		{
-			$router->group(['before' => 'can:manage_users'], function() use ( $router )
-			{
-				$router->get('users', [ 'as' => 'admin.user.index', 'uses' => 'Tlr\Auth\UsersController@index' ]);
-				$router->get('users/{user_id}', [ 'as' => 'admin.user', 'uses' => 'Tlr\Auth\UsersController@show' ]);
-				$router->get('users/{user_id}/edit', [ 'as' => 'admin.user.edit', 'uses' => 'Tlr\Auth\UsersController@edit' ]);
-				$router->put('users/{user_id}/edit', [ 'as' => 'admin.user.update', 'uses' => 'Tlr\Auth\UsersController@update' ]);
-			});
-
-			$router->get('profile', [ 'as' => 'admin.profile', 'uses' => 'Tlr\Auth\UsersController@profile' ]);
-			$router->get('profile/edit', [ 'as' => 'admin.profile.edit', 'uses' => 'Tlr\Auth\UsersController@editProfile' ]);
-			$router->put('profile/edit', [ 'as' => 'admin.profile.update', 'uses' => 'Tlr\Auth\UsersController@updateProfile' ]);
-		});
-
+			$events->fire('routes.admin-prefix', array( $router, $events ));
+		} );
 	}
+
+
+	public function adminPrefixRoutes($router, $events)
+	{
+		$router->group( ['prefix' => 'admin'], function() use ( $router, $events )
+		{
+			$events->fire('routes.admin', array( $router, $events ));
+		} );
+	}
+
+	public function publicRoutes($router, $events)
+	{
+		$events->fire('routes.login-prefix', array( $router, $events ) );
+	}
+
+	public function loginPrefixRoutes($router, $events)
+	{
+		$events->fire('routes.login', array( $router, $events ));
+	}
+
+	public function loginRoutes($router, $events)
+	{
+		$router->get('log/me/in', [ 'as' => 'login', 'uses' => 'Tlr\Auth\LoginController@loginForm' ]);
+		$router->post('i/am/important', [ 'as' => 'login.attempt', 'uses' => 'Tlr\Auth\LoginController@login', 'before' => 'csrf' ]);
+
+		// PASSWORD RESET
+
+		$router->get('reset/password/request', [ 'as' => 'password.request', 'uses' => 'Tlr\Auth\PasswordResetController@request' ]);
+		$router->post('reset/password/request', [ 'as' => 'password.request.process', 'uses' => 'Tlr\Auth\PasswordResetController@processRequest' ]);
+
+		$router->get('reset/password/{token}', [ 'as' => 'password.reset', 'uses' => 'Tlr\Auth\PasswordResetController@reset' ]);
+		$router->post('reset/password', [ 'as' => 'password.reset.process', 'uses' => 'Tlr\Auth\PasswordResetController@processReset' ]);
+	}
+
+	public function privateRoutes($router, $events)
+	{
+		$router->any('logout', [ 'as' => 'logout', 'uses' => 'Tlr\Auth\LoginController@logout' ]);
+	}
+
+	public function adminRoutes($router, $events)
+	{
+		$router->group(['before' => 'can:manage_users'], function() use ( $router )
+		{
+			$router->get('users', [ 'as' => 'admin.user.index', 'uses' => 'Tlr\Auth\UsersController@index' ]);
+			$router->get('users/{user_id}', [ 'as' => 'admin.user', 'uses' => 'Tlr\Auth\UsersController@show' ]);
+			$router->get('users/{user_id}/edit', [ 'as' => 'admin.user.edit', 'uses' => 'Tlr\Auth\UsersController@edit' ]);
+			$router->put('users/{user_id}/edit', [ 'as' => 'admin.user.update', 'uses' => 'Tlr\Auth\UsersController@update' ]);
+		});
+
+		$router->get('profile', [ 'as' => 'admin.profile', 'uses' => 'Tlr\Auth\UsersController@profile' ]);
+		$router->get('profile/edit', [ 'as' => 'admin.profile.edit', 'uses' => 'Tlr\Auth\UsersController@editProfile' ]);
+		$router->put('profile/edit', [ 'as' => 'admin.profile.update', 'uses' => 'Tlr\Auth\UsersController@updateProfile' ]);
+	}
+
 
 	/**
 	 * Get the services provided by the provider.
