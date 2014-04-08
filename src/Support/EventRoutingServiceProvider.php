c42aea1f34427ff96ff26077b61c6426f3e1166d
Stef Horner <shorner@wearearchitect.com>
2014-04-08 09:05:46 +0100

    add routes finish event

--- src/Support/EventRoutingServiceProvider.php
+++ src/Support/EventRoutingServiceProvider.php
@@ -19,7 +19,8 @@ class EventRoutingServiceProvider extends ServiceProvider {
 	{
 		$startRouter = function()
 		{
-			$this->app['events']->fire('routes.start', array( $this->app['router'] ));
+			$this->app['events']->fire('routes.start', array( $this->app['router'], $this->app['events'] ));
+			$this->app['events']->fire('routes.finish', array( $this->app['router'], $this->app['events'] ));
 		};
 
 		$this->app['events']->listen('artisan.start', $startRouter);
